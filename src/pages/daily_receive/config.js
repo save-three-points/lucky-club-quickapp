0232f330d5fb70ab49f40089bb88541e9722b3da
jweboy <1259524459@qq.com>
2021-06-08 00:42:57 +0800

    feat: 新增待领取按钮和步骤说明

---
+++ src/pages/daily_receive/config.js
@@ -0,0 +1,31 @@
+import { ELEME_RED_ENVELOPE_URL, MEITUAN_COUPON_URL } from "../../constants";
+
+export const couponCards = [
+    {
+        title: '外卖红包',
+        titleColor: '#00b6fe',
+        price: {
+            min: 8,
+            max: 30,
+        },
+        toReceived: 1,
+        url: ELEME_RED_ENVELOPE_URL,
+        // btnBgColor: `linear-gradient(90deg, 
+        //     #fa307a 0%, 
+        //     #fd0734 50%, 
+        //     #fd7e41 100%)`
+    },
+    {
+        title: '天天都能领',
+        titleColor: '#fcc85d',
+        price: {
+            max: 38,
+        },
+        toReceived: 2,
+        url: MEITUAN_COUPON_URL,
+        // btnBgColor: `linear-gradient(90deg, 
+        //     #e43939 0%, 
+        //     #d42020 46%, 
+        //     #ea0505 100%)`
+    },
+]
